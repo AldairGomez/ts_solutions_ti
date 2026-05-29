c79444b733eefbcc62920930b56b0e296971e10d
AldairGomez25 <aldairgomezc25@gmail.com>
2026-05-28 21:09:20 -0500

    cambio para visualizacion en dominio

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/ts_solutions_ti/',
+  base: '/',
 })
